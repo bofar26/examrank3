8630d548dd194f56d2ec577e432cf6c4cf828c99
bofar26 <bofar26@gmail.com>
2025-11-12 18:16:58 +0100

    exam passed

--- exam rank3/prarctise/n_queen/n_queen.c
+++ exam rank3/prarctise/n_queen/n_queen.c
@@ -1,29 +1,27 @@
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-void	put_words(int *pos, int n, int *found)
+void	put_words(int *pos, int n)
 {
-	int i;
+	int	i;
 
-	for (i = 0; i < n; i ++)
+	for (i = 0; i < n; i++)
 	{
 		if (i)
 			fprintf(stdout, " ");
 		fprintf(stdout, "%d", pos[i]);
 	}
 	fprintf(stdout, "\n");
-	*found = 1;
 }
 
-void	dfs(int *pos, int col, int n, int *used_col, int *used_d1, int *used_d2, int *found)
+void	dfs(int *pos, int col, int n, int *used_col, int *used_d1, int *used_d2)
 {
 	int	row;
 	int	d1, d2;
 
 	if (col == n)
 	{
-		put_words(pos, n, found);
+		put_words(pos, n);
 		return ;
 	}
 	for (row = 0; row < n; row ++)
@@ -36,7 +34,7 @@ void	dfs(int *pos, int col, int n, int *used_col, int *used_d1, int *used_d2, in
 			used_col[row] = 1;
 			used_d1[d1] = 1;
 			used_d2[d2] = 1;
-			dfs(pos, col + 1, n, used_col, used_d1, used_d2, found);
+			dfs(pos, col + 1, n, used_col, used_d1, used_d2);
 			used_col[row] = 0;
 			used_d1[d1] = 0;
 			used_d2[d2] = 0;
@@ -46,24 +44,17 @@ void	dfs(int *pos, int col, int n, int *used_col, int *used_d1, int *used_d2, in
 
 int	main(int argc, char **argv)
 {
-	int	n;
-
 	if (argc != 2 || !*argv[1])
 		return (1);
-	n = atoi(argv[1]);
-	int	used_col[n];
+	int n = atoi(argv[1]);
 	int	pos[n];
-	int	used_d1[2 * n];
-	int	used_d2[2 * n];
-	int	found = 0;
-
-	int i = 0;
-	for(i = 0; i < n; i ++)
+	int	used_col[n];
+	int	used_d1[2 * n - 1];
+	int	used_d2[2 * n - 1];
+	for (int i = 0; i < n; i ++)
 		used_col[i] = 0;
-	for(int j = 0; j < 2 * n; j ++)
-		used_d1[j] = used_d2[j] = 0;
-	dfs(pos, 0, n, used_col, used_d1, used_d2, &found);
-	if (found == 0)
-		fprintf(stdout, "\n");
+	for (int m = 0; m < 2*n - 1; m ++)
+		used_d1[m] = used_d2[m] = 0;
+	dfs(pos, 0, n, used_col, used_d1, used_d2);
 	return (0);
 }
--- exam rank3/prarctise/powerset/powerset.c
+++ exam rank3/prarctise/powerset/powerset.c
@@ -1,13 +1,12 @@
 #include <stdlib.h>
-#include <unistd.h>
 #include <stdio.h>
 
 void	put_words(int *arr, int n, int *pick, int *found)
 {
-	int	i = 0;
+	int	i;
 	int	first = 1;
 
-	for (i = 0; i < n; i ++)
+	for(i = 0; i < n; i++)
 	{
 		if (pick[i])
 		{
@@ -21,7 +20,7 @@ void	put_words(int *arr, int n, int *pick, int *found)
 	*found = 1;
 }
 
-void	dfs(int	*arr, int n, int index, int sum, int target, int *pick, int *found)
+void	dfs(int *arr, int n, int index, int sum, int target, int *pick, int *found)
 {
 	if (index == n)
 	{
@@ -30,42 +29,34 @@ void	dfs(int	*arr, int n, int index, int sum, int target, int *pick, int *found)
 		return ;
 	}
 	pick[index] = 0;
-	dfs(arr, n , index + 1, sum, target, pick, found);
+	dfs(arr, n, index + 1, sum, target, pick, found);
 	pick[index] = 1;
 	dfs(arr, n, index + 1, sum + arr[index], target, pick, found);
 }
+
 int	main(int argc, char **argv)
 {
+	int	n;
 	int	*arr;
 	int	*pick;
-	int	n;
 	int	target;
-	int found = 0;
+	int	found;
 
 	if (argc < 2 || !*argv[1])
-	{
-		printf("\n");
 		return (1);
-	}
+	found = 0;
 	n = argc - 2;
 	target = atoi(argv[1]);
-	arr = (int *)malloc(sizeof(int) * n);
-	pick = (int *)calloc(sizeof(int), n);
+	arr = (int *)malloc(n * sizeof(int));
+	pick = (int *)calloc(n, sizeof(int));
 	if (!arr || !pick)
-	{
-		printf("\n");
 		return (1);
-	}
-	int i = 0;
-	while (i < n)
-	{
+	for (int i = 0; i < n; i ++)
 		arr[i] = atoi(argv[i + 2]);
-		i ++;
-	}
 	dfs(arr, n, 0, 0, target, pick, &found);
 	free(arr);
 	free(pick);
 	if (found == 0)
-		return (printf("\n"), 0);
+		printf("\n");
 	return (0);
 }
